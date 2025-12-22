d01836f6d4385e6900f08bec326ba6a4f87621ca
NataSeidor <nespinoza@seidor.es>
2025-12-22 17:29:59 -0500

    change imagen hero

--- src/islands/ContactForm.jsx
+++ src/islands/ContactForm.jsx
@@ -119,9 +119,9 @@ export default function ContactForm() {
   /* ================== UI ================== */
 
   return (
-    <section className="bg-bg-variant py-20">
+    <section className="bg-bg-variant py-20 section-contact">
       <Card className="max-w-md mx-auto bg-panel shadow-xl rounded-base">
-        <CardHeader className="space-y-3 text-h1">
+        <CardHeader className="space-y-3 text-h3">
           <CardTitle className="font-head text-h2 leading-tight text-text">
             ¿Cómo podemos contactarte?
           </CardTitle>
