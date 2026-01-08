9b49c5d2b1faa83a721ddf12666b3b392ca21f7b
Kengya Moncada <kmoncada@seidor.es>
2026-01-08 10:40:21 -0500

    feat(i18n): centraliza traducciones con localeHelper y paths dinámicos

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -2,11 +2,20 @@ import es from '../i18n/es.json'
 import en from '../i18n/en.json'
 
 const messages = { es, en }
-
-export const getT = (locale) => (key) => messages[locale][key] ?? key
+const getT = (locale) => (key) => messages[locale][key] ?? key
 
 export const localeHelper = (Astro) => {
-  const locale = Astro.currentLocale || 'es'
+  const locale = Astro.currentLocale ?? 'es'
+  const base = import.meta.env.BASE_URL
+  
   const t = getT(locale)
-  return { locale, t }
+  /**url puede ser '' para home */
+  const path = (url = '') => `${base}${locale}/${url}/`.replace(/\/+/g, '/')
+
+  console.log({
+    base: import.meta.env.BASE_URL,
+    locale: Astro.currentLocale,
+  })
+
+  return { locale, t, path }
 }
