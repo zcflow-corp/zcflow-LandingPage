309cee4c99f4c8d35289661c2077bfdd361d685b
NataSeidor <nespinoza@seidor.es>
2026-02-04 16:24:35 -0500

    add standole

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,18 @@ import { defineConfig } from 'astro/config'
 import { fileURLToPath } from 'node:url'
 import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
+import node from '@astrojs/node' // 👈 ESTA LÍNEA FALTABA
 
 const isProd = process.env.NODE_ENV === 'production'
 
 export default defineConfig({
   site: isProd ? 'https://zcflow-corp.github.io' : 'http://localhost:4321',
+  output: 'server',
   base: isProd ? '/zcflow-LandingPage/' : '/',
+  adapter: node({
+    mode: 'standalone',
+  }),
+
   server: {
     allowedHosts: ['porky-nonpossessively-kimberlie.ngrok-free.dev', 'localhost'],
   },
